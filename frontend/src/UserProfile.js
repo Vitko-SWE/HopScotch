7b9e17ec6e72920928710c7f21a3814ddd8d7923
unknown <wu1272@purdue.edu>
2021-02-12 21:43:32 -0500

    ugly profile pic upload feature, no functionality atm

--- frontend/src/UserProfile.js
+++ frontend/src/UserProfile.js
@@ -15,11 +15,9 @@ class UserProfile extends React.Component {
     }
 
     //For later... save photo into DB somehow
-    //https://www.youtube.com/watch?v=XeiOnkEI7XI
     fileUploadHandler = () => {
         axios.post('');
     }
-
 
     render() {
         return (
@@ -31,6 +29,9 @@ class UserProfile extends React.Component {
                 <h3>About Dude</h3>
                 <p>October arrived, spreading a damp chill over the grounds and into the castle. Madam Pomfrey, the nurse, was kept busy by a sudden spate of colds...</p>
 
+                <h3>Change Profile Picture</h3>
+                <input type="file" onChange={this.fileSelectedHandler} /><br></br>
+                <button className="hbtn hb-border-top-br3" onClick={this.fileUploadHandler}>Upload</button><br></br>
                 <button className="hbtn hb-border-top-br3" onClick={() => window.location.href = '/'}>Home</button>
 
             </div>
