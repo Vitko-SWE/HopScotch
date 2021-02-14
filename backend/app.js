5f7018cc960be85a8d072fd487cb2c0a980bc9d8
Vitalii Kovtunenko <vkovtoun@purdue.edu>
2021-02-14 16:26:16 -0500

    Added code snippet in backend app.js to test data retrieval from db

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const db = require('./db.js');
 
+
+
 const app = express();
 
 app.use(express.urlencoded());
 app.use(express.json());
+app.use(express.query())
 
 if (process.env.NODE_ENV == "production") {
     const publicPath = path.join(__dirname, './frontend');
@@ -12,5 +15,20 @@ if (process.env.NODE_ENV == "production") {
     app.use('*', express.static(publicPath));
 }
 
+app.get('/hopscotch', (req, res) => {
+
+    db.execute('SELECT * FROM Trip WHERE TripID=1', (err,rows) => {
+        if(err) throw err;
+      
+        console.log('Data received from Db:');
+        console.log(rows);
+        res.send(rows)
+      });
+})
+
+// getTripByID()
+
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+
