9727afe6e626ac0ae520bf1b6c61ffdee2b0fadb
neelc17 <neelch1154@gmail.com>
2021-02-16 11:37:21 -0500

    install router, added routing to App.js, and moved navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
-import Homepage from './Components/Homepage/Homepage.js'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import MenuBar from './Components/MenuBar/MenuBar.js';
+import Homepage from './Components/Homepage/Homepage.js';
 
 function App() {
   return (
-    <div className="App">
-      <Homepage/>
-    </div>
+    <Router>
+      <div className="App">
+        <MenuBar />
+
+        <Switch>
+          <Route path="/example">
+            <div style={{marginTop: 100+"px"}}>
+              Hello World!
+            </div>
+          </Route>
+          <Route path="/">
+            <Homepage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/Components/Homepage/Homepage.js
+++ frontend/src/Components/Homepage/Homepage.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import MenuBar from '../MenuBar/MenuBar.js'
 import TripCards from '../TripCard/TripCard'
 class homepage extends Component {
-    render() {  
+    render() {
         return (
             <div style={{height: '100%'}}>
-                <MenuBar/>
                 <main style={{marginTop: '64px'}}>
                     <TripCards/>
                 </main>
