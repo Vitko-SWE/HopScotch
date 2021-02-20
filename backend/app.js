134e598482daba4219a89a63d95d18f545ef94de
Jack Doherty <doherty9@purdue.edu>
2021-02-20 03:05:14 -0500

    implemeted basic API auth

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const db = require('./db.js');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+require('dotenv').config()
 
 const app = express();
+
+const jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://flyhopscotch-dev.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: process.env.AUTH0_AUDIENCE,
+    issuer: process.env.AUTH0_ISSUER,
+    algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
 
 app.use(express.urlencoded());
 app.use(express.json());
@@ -11,6 +28,10 @@ if (process.env.NODE_ENV == "production") {
     app.use(express.static(publicPath));
     app.use('*', express.static(publicPath));
 }
+
+app.get('/authorized', (req, res) => {
+    res.send('Secured Resource');
+})
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
