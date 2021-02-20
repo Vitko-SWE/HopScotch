ae35d6f202a63c483e311aba99a62bcd35a06a5d
Jack Doherty <doherty9@purdue.edu>
2021-02-19 23:22:51 -0500

    crude log in/out created on frontend

--- frontend/src/Components/Landing/Landing.js
+++ frontend/src/Components/Landing/Landing.js
@@ -1,21 +1,28 @@
-import React, { Component } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import React from "react";
+import LoginButton from "../LoginButton";
+import LogoutButton from "../LogoutButton"
 import "./Landing.css";
 
-class Landing extends Component {
-  constructor(props) {
-    super(props);
-  }
+export default function Landing() {
+  const { user, isAuthenticated } = useAuth0();
 
-  render() {
-    return(
-      <div>
-        <br />
-        <br />
-        <br />
-        <p>Welcome to Hopscotch!</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <br />
+      <br />
+      <br />
+      <p>Welcome to Hopscotch!</p>
+      <LoginButton />
+      <LogoutButton />
+      <br />
+      {isAuthenticated && (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      )}
+    </div>
+  )
 }
-
-export default Landing;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,8 +9,8 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <Auth0Provider
-    domain={`${process.env.REACT_APP_AUTH0_DOMAIN}`}
-    clientId={`${process.env.REACT_APP_AUTH0_CLIENT_ID}`}
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     redirectUri={window.location.origin}
   >
     <React.StrictMode>
