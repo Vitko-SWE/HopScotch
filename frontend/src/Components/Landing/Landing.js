f870f08e83bb9bb95e8b46378cac261e3093fd76
neelc17 <neelch1154@gmail.com>
2021-02-23 14:53:59 -0500

    added logout button and authentication based changing to navbar

--- frontend/src/Components/Landing/Landing.js
+++ frontend/src/Components/Landing/Landing.js
@@ -8,20 +8,25 @@ export default function Landing() {
   const { user, isAuthenticated } = useAuth0();
 
   return (
-    <div class="container">
-      <h1>Welcome to Hopscotch!</h1>
-      <p class="para-center mt-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros enim, condimentum eu faucibus et, hendrerit non urna. Suspendisse vitae convallis libero. Aliquam tempus lacus et augue congue euismod. Sed eleifend tristique sapien eu iaculis. Integer quis auctor felis. Nam volutpat ante ac ipsum rutrum dapibus. Proin lobortis diam at arcu maximus cursus. Etiam sit amet lacus rhoncus, auctor velit et, faucibus justo.</p>
-      {!isAuthenticated && (
-        <LoginButton btntext="Get Started" />
-      )}
-      {isAuthenticated && (
-        <div class="mt-5">
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-          <LogoutButton />
-        </div>
-      )}
+    <div>
+      <div class="intro pb-5">
+        <h1>Welcome to Hopscotch!</h1>
+        <p class="para-center mt-5 mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eros enim, condimentum eu faucibus et, hendrerit non urna. Suspendisse vitae convallis libero. Aliquam tempus lacus et augue congue euismod. Sed eleifend tristique sapien eu iaculis. Integer quis auctor felis. Nam volutpat ante ac ipsum rutrum dapibus. Proin lobortis diam at arcu maximus cursus. Etiam sit amet lacus rhoncus, auctor velit et, faucibus justo.</p>
+        {!isAuthenticated && (
+          <div><LoginButton btntext="Get Started" /></div>
+        )}
+        {isAuthenticated && (
+          <div class="mt-5">
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <LogoutButton />
+          </div>
+        )}
+      </div>
+      <div>
+        <h1>Yooo hahah</h1>
+      </div>
     </div>
-  )
+  );
 }
--- frontend/src/Components/MenuBar/MenuBar.js
+++ frontend/src/Components/MenuBar/MenuBar.js
@@ -1,8 +1,12 @@
-import React from 'react'
-import './MenuBar.css'
+import React from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import './MenuBar.css';
+import LogoutButton from "../LogoutButton"
 
+export default function MenuBar() {
+  const { user, isAuthenticated } = useAuth0();
 
-const menubar = props => (
+  return (
     <header className="menubar sticky-nav">
         <nav className="menubar_navigation">
             <div className="menubar_logo"><a href="/">Hopscotch | My Trips</a></div>
@@ -10,11 +14,15 @@ const menubar = props => (
             <div className="menubar_nav_items">
                 <ul>
                     <li><a href="/">Search</a></li>
-                    <li><a href="/">Profile Picture</a></li>
+                    {isAuthenticated && (
+                      <li><a href="/">Hi, {user.name}!</a></li>
+                    )}
+                    {isAuthenticated && (
+                      <li><LogoutButton /></li>
+                    )}
                 </ul>
             </div>
         </nav>
     </header>
-)
-
-export default menubar
+  );
+};
