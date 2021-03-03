53b5d0b16f890217b6aeb32a17957da4dbf0daa5
Jack Doherty <doherty9@purdue.edu>
2021-03-03 10:45:01 -1000

    added basic smoke test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,5 +2,6 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { container } = render(<App />);
+  const { baseElement } = render(<App />);
+  expect(baseElement).toBeInTheDocument();
 });
