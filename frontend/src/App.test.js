ff34bfd891b732b9ed4bbd980b70e1186c2c0923
Jack Doherty <doherty9@purdue.edu>
2021-03-03 09:59:46 -1000

    demo frontend tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,6 +2,5 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  const appelem = render(<App />);
-  expect(appelem.baseElement).toBeInTheDocument();
+  const { container } = render(<App />);
 });
--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { setupServer } from "msw/node";
+
+const server = setupServer(
+    rest.get('/mswtest', (req, res, ctx) => {
+        return res(ctx.json({ greeting: "hello there" }))
+    })
+)
+
+beforeAll(() => server.listen())
