f3570b8b6efd167ee013a0d9fa92cc1416f73bd2
Jack Doherty <doherty9@purdue.edu>
2021-03-03 10:47:36 -1000

    title fix

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('smoke test', () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeInTheDocument();
 });
