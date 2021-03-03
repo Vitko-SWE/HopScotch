0e54435bb34019e4adcf1a9640299b0bd1e6b93b
Jack Doherty <doherty9@purdue.edu>
2021-03-03 10:35:47 -1000

    added sample test using msw

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,12 +3,8 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
-import { setupServer } from "msw/node";
-
-const server = setupServer(
-    rest.get('/mswtest', (req, res, ctx) => {
-        return res(ctx.json({ greeting: "hello there" }))
-    })
-)
+import { server } from './mocks/server';
 
 beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
