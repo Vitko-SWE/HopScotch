9c7526e73ac4026be1b3f0bdbd5085f8a8426f72
neelc17 <neelch1154@gmail.com>
2021-03-18 00:51:31 -0400

    added poi search and filtering

--- backend/routes/searches.js
+++ backend/routes/searches.js
@@ -3,23 +3,64 @@ const db = require('../db');
 const axios = require("axios");
 let router = express.Router()
 
-router.route("/attractionsearch/:query").get((req, res) => {
+router.route("/attractionsearch").post((req, res) => {
   axios.post("https://test.api.amadeus.com/v1/security/oauth2/token", "grant_type=client_credentials&client_id=Q4aEVTArfMixbGwELBlPoHgWPmy4PmTR&client_secret=cXMQ7qdUH24shfpA", {
     headers: {
       Content_Type: "application/x-www-form-urlencoded",
     },
   }).then((resp) => {
     const token = resp.data.access_token;
-    axios.get("https://test.api.amadeus.com/v1/shopping/activities?latitude=41.397158&longitude=2.160873&radius=1", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    }).then((respo) => {
-      res.send(respo.data);
-    }).catch((err) => {
-      console.log(err);
-      res.send(err);
-    });
+
+    if (req.body.filter === "Points of Interest") {
+      axios.get(`https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude=${req.body.latitude}&longitude=${req.body.longitude}&radius=20`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((respon) => {
+        const fr = {
+          ta: [],
+          poi: respon.data.data,
+        };
+        res.send(fr);
+      }).catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+    }
+    else {
+      axios.get(`https://test.api.amadeus.com/v1/shopping/activities?latitude=${req.body.latitude}&longitude=${req.body.longitude}&radius=20`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }).then((respo) => {
+        if (req.body.filter === "All") {
+          axios.get(`https://test.api.amadeus.com/v1/reference-data/locations/pois?latitude=${req.body.latitude}&longitude=${req.body.longitude}&radius=20`, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }).then((respon) => {
+            const fr = {
+              ta: respo.data.data,
+              poi: respon.data.data,
+            };
+            res.send(fr);
+          }).catch((err) => {
+            console.log(err);
+            res.send(err);
+          });
+        }
+        else {
+          const fr = {
+            ta: respo.data.data,
+            poi: [],
+          };
+          res.send(fr);
+        }
+      }).catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+    }
   }).catch((err) => {
     console.log(err);
     res.send(err);
--- frontend/src/Components/AttractionSearch/AttractionSearch.js
+++ frontend/src/Components/AttractionSearch/AttractionSearch.js
@@ -3,23 +3,30 @@ import { Form, Button, Card } from "react-bootstrap";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import "../TripCard/Card.css"
+import "./Card.css"
 
 export default function AttractionSearch() {
   const { getAccessTokenSilently } = useAuth0();
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchResults, setSearchResults] = useState({
+    ta: [],
+    poi: [],
+  });
 
   const handleSearch = (e) => {
     e.preventDefault();
-    const value = e.currentTarget.searchQuery.value;
+    const value = e.currentTarget;
     getAccessTokenSilently({ audience: "https://hopscotch/api" }).then((res) => {
-      axios.get(`/api/searches/attractionsearch/${encodeURIComponent(value)}`, {
+      axios.post("/api/searches/attractionsearch/", {
+        latitude: value.searchLatitude.value,
+        longitude: value.searchLongitude.value,
+        query: value.searchQuery.value,
+        filter: value.searchFilter.value,
+      }, {
         headers: {
           Authorization: `Bearer ${res}`,
         },
       }).then((res) => {
-        //console.log(res.data);
-        setSearchResults(res.data.data);
+        setSearchResults(res.data);
       }).catch((err) => {
         console.log(err);
       });
@@ -30,23 +37,55 @@ export default function AttractionSearch() {
     <div>
       <h1>Attraction Search</h1>
       <Form onSubmit={handleSearch}>
+        <Form.Group controlId="searchLatitude">
+          <Form.Control required />
+        </Form.Group>
+        <Form.Group controlId="searchLongitude">
+          <Form.Control required />
+        </Form.Group>
         <Form.Group controlId="searchQuery">
           <Form.Control required />
         </Form.Group>
+        <Form.Group controlId="searchFilter">
+          <Form.Control as="select" required>
+            <option>All</option>
+            <option>Tours and Activities</option>
+            <option>Points of Interest</option>
+          </Form.Control>
+        </Form.Group>
         <Button variant="primary" type="submit">Search</Button>
       </Form>
-      <div className="custom_container">
-      {searchResults.map((result) => (
-        <Card className="custom_card">
-          <Card.Img variant="top" src={result.pictures[0]} />
-          <Card.Body>
-            <Card.Title>{result.name}</Card.Title>
-            <Card.Text>{result.shortDescription}</Card.Text>
-            <Card.Text><a href={result.bookingLink}>Booking Link</a></Card.Text>
-          </Card.Body>
-        </Card>
-      ))}
-      </div>
+      {searchResults.ta.length !== 0 && (
+        <div>
+          <h3>Tours and Activities</h3>
+          <div className="custom_container">
+            {searchResults.ta.map((result) => (
+              <Card className="custom_card">
+                <Card.Img variant="top" src={result.pictures[0]} />
+                <Card.Body>
+                  <Card.Title>{result.name}</Card.Title>
+                  <Card.Text><a href={result.bookingLink}>Booking Link</a></Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        </div>
+      )}
+      {searchResults.poi.length !== 0 && (
+        <div>
+          <h3>Points of Interest</h3>
+          <div className="custom_container">
+            {searchResults.poi.map((result) => (
+              <Card className="custom_card">
+                <Card.Body>
+                  <Card.Title>{result.name}</Card.Title>
+                  <Card.Text>Type: {result.category}</Card.Text>
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
