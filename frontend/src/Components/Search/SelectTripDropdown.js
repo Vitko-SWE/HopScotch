881406c6b979e49b49121c3a30fb356b4ddb13f8
mhunterdavid <mhd1998@aol.com>
2021-04-01 20:38:37 -0400

    Added initial vote for everything but flight, fixed vote cards

--- frontend/src/Components/Search/SelectTripDropdown.js
+++ frontend/src/Components/Search/SelectTripDropdown.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 import axios from 'axios'
 import { Dropdown, DropdownButton } from 'react-bootstrap'
+import { useHistory } from 'react-router';
+
 export default function SelectTripDropdown(props) {
     const {user, getAccessTokenSilently} = useAuth0();
     const trips = useState({items: []});
+    const history = useHistory();
     
     const handleSelect = (item) => {
         console.log(item.TripId)
@@ -16,11 +19,27 @@ export default function SelectTripDropdown(props) {
         }
 
         getAccessTokenSilently({ audience: "https://hopscotch/api" }).then((res) => {
+            const authToken = res;
             axios.post('/api/search/selectDining', newFeature, {
                 headers: {
                 Authorization: `Bearer ${res}`,
                 },
             }).then((res) => {
+                axios.post("/api/trips/vote", {
+                    tripid: item.TripId,
+                    userid: user.sub,
+                    featureid: props.diningOption.id,
+                    isflight: 0,
+                    score: 1
+                }, {
+                    headers: {
+                        Authorization: `Bearer ${authToken}`
+                    }
+                }).then(res3 => {
+                })
+                .catch((err) =>{
+                    console.log(err);
+                });
                 alert("The dining option has been added to your trip!")
             }).catch((err) => {
                 console.log(err);
