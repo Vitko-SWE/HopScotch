aa706929310c5bafca51889e59f33f7bafa5479d
Jack Doherty <doherty9@purdue.edu>
2021-04-30 00:19:53 -0400

    airport auto complete YEEET

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-typeahead/css/Typeahead.css';
 
 import { Auth0Provider } from "@auth0/auth0-react";
 
